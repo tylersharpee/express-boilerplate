1637ce2c62774debab1573e65e178723eb86bff8
Tyler Sharpee <sharpee94@gmail.com>
2020-10-22 12:07:47 -0400

    config.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const {NODE_ENV} = require('./config');
 
 const app = express();
 
-const morganOption = (process.env.NODE_ENV === 'production')
+const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
@@ -20,7 +21,7 @@ app.get('/', (req,res) => {
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (process.env.NODE_ENV ==='production') {
+  if (NODE_ENV ==='production') {
     response = {error: { message: 'serer error'}};
   }
   else {

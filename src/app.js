1b869aa13cf00e9789fd77196122a2b3ce2a8adb
Tyler Sharpee <sharpee94@gmail.com>
2020-10-22 12:02:59 -0400

    error handling

--- src/app.js
+++ src/app.js
@@ -18,4 +18,16 @@ app.get('/', (req,res) => {
   res.send('Hello world!');
 });
 
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (process.env.NODE_ENV ==='production') {
+    response = {error: { message: 'serer error'}};
+  }
+  else {
+    console.error(error);
+    response = {message: error.message, error};
+  }
+  res.status(500).json(response);
+});
+
 module.exports = app;

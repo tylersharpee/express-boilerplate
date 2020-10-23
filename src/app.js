ef9536594ddf44dc7ee8db2fb965d9c54b932dd2
Tyler Sharpee <sharpee94@gmail.com>
2020-10-23 11:23:04 -0400

    correct syntax errors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-app.us(express.json());
+app.use(express.json());
 
 
 // Routes
